ceee992ef00a7239d946e34464c4dffc59395b27
Arunava Kundu <42401932+arunava3798@users.noreply.github.com>
2019-01-19 20:11:33 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,427 @@
+var tbill=0;
+var flag=0;
+function openNav() 
+	{
+    	document.getElementById("mySidebar").style.width = "250px";
+    	document.getElementById("main").style.marginLeft = "250px";
+	}
+function closeNav() 
+	{
+    	document.getElementById("mySidebar").style.width = "0";
+    	document.getElementById("main").style.marginLeft= "0";
+	}
+
+
+function myfunctionM1()
+{
+	tbill+=499.00;
+	if(flag==0)
+	{
+		var access1 = document.getElementById("itemM1");
+		var access2=document.getElementById("priceM1");
+	}
+	else if(flag==1)
+	{
+		var access1 = document.getElementById("itemM2");
+		var access2=document.getElementById("priceM2");
+	}
+	else if(flag==2)
+	{
+		var access1 = document.getElementById("itemM3");
+		var access2=document.getElementById("priceM3");
+	}
+	else if(flag==3)
+	{
+		var access1 = document.getElementById("itemM4");
+		var access2=document.getElementById("priceM4");
+	}
+	else if(flag==4)
+	{
+		alert("Please checkout to continue....")
+	}
+	var name="Item : Men's blue printed Shirt";
+	access1.innerHTML = name;
+	access2.innerHTML= "499.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag++;
+}
+function myfunctionM2()
+{
+	tbill+=699.00;
+	if(flag==0)
+	{
+		var access1 = document.getElementById("itemM1");
+		var access2=document.getElementById("priceM1");
+	}
+	else if(flag==1)
+	{
+		var access1 = document.getElementById("itemM2");
+		var access2=document.getElementById("priceM2");
+	}
+	else if(flag==2)
+	{
+		var access1 = document.getElementById("itemM3");
+		var access2=document.getElementById("priceM3");
+	}
+	else if(flag==3)
+	{
+		var access1 = document.getElementById("itemM4");
+		var access2=document.getElementById("priceM4");
+	}
+	else if(flag==4)
+	{
+		alert("Please checkout to continue....")
+	}
+	var name="Item : Men's Multicolor Shirt";
+	access1.innerHTML = name;
+	access2.innerHTML= "699.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag++;
+}
+function myfunctionM3()
+{
+	tbill+=599.00;
+	if(flag==0)
+	{
+		var access1 = document.getElementById("itemM1");
+		var access2=document.getElementById("priceM1");
+	}
+	else if(flag==1)
+	{
+		var access1 = document.getElementById("itemM2");
+		var access2=document.getElementById("priceM2");
+	}
+	else if(flag==2)
+	{
+		var access1 = document.getElementById("itemM3");
+		var access2=document.getElementById("priceM3");
+	}
+	else if(flag==3)
+	{
+		var access1 = document.getElementById("itemM4");
+		var access2=document.getElementById("priceM4");
+	}
+	else if(flag==4)
+	{
+		alert("Please checkout to continue....")
+	}
+	var name="Item : Men's Navy blue Shirt";
+	access1.innerHTML = name;
+	access2.innerHTML= "599.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag++;
+}
+function myfunctionM4()
+{
+	tbill+=399.00;
+	if(flag==0)
+	{
+		var access1 = document.getElementById("itemM1");
+		var access2=document.getElementById("priceM1");
+	}
+	else if(flag==1)
+	{
+		var access1 = document.getElementById("itemM2");
+		var access2=document.getElementById("priceM2");
+	}
+	else if(flag==2)
+	{
+		var access1 = document.getElementById("itemM3");
+		var access2=document.getElementById("priceM3");
+	}
+	else if(flag==3)
+	{
+		var access1 = document.getElementById("itemM4");
+		var access2=document.getElementById("priceM4");
+	}
+	else if(flag==4)
+	{
+		alert("Please checkout to continue....")
+	}
+	var name="Item : Men's devotional Tshirt";
+	access1.innerHTML = name;
+	access2.innerHTML= "399.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag++;
+}
+function myfunctionM5()
+{
+	tbill+=499.00;
+	if(flag==0)
+	{
+		var access1 = document.getElementById("itemM1");
+		var access2=document.getElementById("priceM1");
+	}
+	else if(flag==1)
+	{
+		var access1 = document.getElementById("itemM2");
+		var access2=document.getElementById("priceM2");
+	}
+	else if(flag==2)
+	{
+		var access1 = document.getElementById("itemM3");
+		var access2=document.getElementById("priceM3");
+	}
+	else if(flag==3)
+	{
+		var access1 = document.getElementById("itemM4");
+		var access2=document.getElementById("priceM4");
+	}
+	else if(flag==4)
+	{
+		alert("Please checkout to continue....")
+	}
+	var name="Item : Men's Multicolor Tshirt";
+	access1.innerHTML = name;
+	access2.innerHTML= "499.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag++;
+}
+function myfunctionM6()
+{
+	tbill+=599.00;
+	if(flag==0)
+	{
+		var access1 = document.getElementById("itemM1");
+		var access2=document.getElementById("priceM1");
+	}
+	else if(flag==1)
+	{
+		var access1 = document.getElementById("itemM2");
+		var access2=document.getElementById("priceM2");
+	}
+	else if(flag==2)
+	{
+		var access1 = document.getElementById("itemM3");
+		var access2=document.getElementById("priceM3");
+	}
+	else if(flag==3)
+	{
+		var access1 = document.getElementById("itemM4");
+		var access2=document.getElementById("priceM4");
+	}
+	else if(flag==4)
+	{
+		alert("Please checkout to continue....")
+	}
+	var name="Item : Men's Color Blended Tshirt";
+	access1.innerHTML = name;
+	access2.innerHTML= "599.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag++;
+}
+function myfunctionM7()
+{
+	tbill+=499.00;
+	if(flag==0)
+	{
+		var access1 = document.getElementById("itemM1");
+		var access2=document.getElementById("priceM1");
+	}
+	else if(flag==1)
+	{
+		var access1 = document.getElementById("itemM2");
+		var access2=document.getElementById("priceM2");
+	}
+	else if(flag==2)
+	{
+		var access1 = document.getElementById("itemM3");
+		var access2=document.getElementById("priceM3");
+	}
+	else if(flag==3)
+	{
+		var access1 = document.getElementById("itemM4");
+		var access2=document.getElementById("priceM4");
+	}
+	else if(flag==4)
+	{
+		alert("Please checkout to continue....")
+	}
+	var name="Item : Men's Running Shoes";
+	access1.innerHTML = name;
+	access2.innerHTML= "499.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag++;
+}
+function myfunctionM8()
+{
+	tbill+=699.00;
+	if(flag==0)
+	{
+		var access1 = document.getElementById("itemM1");
+		var access2=document.getElementById("priceM1");
+	}
+	else if(flag==1)
+	{
+		var access1 = document.getElementById("itemM2");
+		var access2=document.getElementById("priceM2");
+	}
+	else if(flag==2)
+	{
+		var access1 = document.getElementById("itemM3");
+		var access2=document.getElementById("priceM3");
+	}
+	else if(flag==3)
+	{
+		var access1 = document.getElementById("itemM4");
+		var access2=document.getElementById("priceM4");
+	}
+	else if(flag==4)
+	{
+		alert("Please checkout to continue....")
+	}
+	var name="Item : Men's Brown Running shoes";
+	access1.innerHTML = name;
+	access2.innerHTML= "699.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag++;
+}
+function myfunctionM9()
+{
+	tbill+=499.00;
+	if(flag==0)
+	{
+		var access1 = document.getElementById("itemM1");
+		var access2=document.getElementById("priceM1");
+	}
+	else if(flag==1)
+	{
+		var access1 = document.getElementById("itemM2");
+		var access2=document.getElementById("priceM2");
+	}
+	else if(flag==2)
+	{
+		var access1 = document.getElementById("itemM3");
+		var access2=document.getElementById("priceM3");
+	}
+	else if(flag==3)
+	{
+		var access1 = document.getElementById("itemM4");
+		var access2=document.getElementById("priceM4");
+	}
+	else if(flag==4)
+	{
+		alert("Please checkout to continue....")
+	}
+	var name="Item : Men's Blue Laceless Shoes";
+	access1.innerHTML = name;
+	access2.innerHTML= "499.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag++;
+}
+function myfunctionM10()
+{
+	tbill+=1199.00;
+	if(flag==0)
+	{
+		var access1 = document.getElementById("itemM1");
+		var access2=document.getElementById("priceM1");
+	}
+	else if(flag==1)
+	{
+		var access1 = document.getElementById("itemM2");
+		var access2=document.getElementById("priceM2");
+	}
+	else if(flag==2)
+	{
+		var access1 = document.getElementById("itemM3");
+		var access2=document.getElementById("priceM3");
+	}
+	else if(flag==3)
+	{
+		var access1 = document.getElementById("itemM4");
+		var access2=document.getElementById("priceM4");
+	}
+	else if(flag==4)
+	{
+		alert("Please checkout to continue....")
+	}
+	var name="Item : Men's Brown Leather Shoes";
+	access1.innerHTML = name;
+	access2.innerHTML= "1199.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag++;
+}
+function reset1()
+{
+	var item1=document.getElementById("itemM1");
+	var price1=document.getElementById("priceM1");
+	price=price1.innerHTML;
+	tbill-=price;
+	var item2=document.getElementById("itemM2");
+	var price2=document.getElementById("priceM2");
+	var item3=document.getElementById("itemM3");
+	var price3=document.getElementById("priceM3");
+	var item4=document.getElementById("itemM4");
+	var price4=document.getElementById("priceM4");
+	item1.innerHTML=item2.innerHTML;
+	price1.innerHTML=price2.innerHTML;
+	item2.innerHTML=item3.innerHTML;
+	price2.innerHTML=price3.innerHTML;
+	item3.innerHTML=item4.innerHTML;
+	price3.innerHTML=price4.innerHTML;
+	item4.innerHTML="Item : Empty";
+	price4.innerHTML="0.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag--;
+	
+}
+function reset2()
+{
+	var item2=document.getElementById("itemM2");
+	var price2=document.getElementById("priceM2");
+	price=price2.innerHTML;
+	tbill-=price;
+	var item3=document.getElementById("itemM3");
+	var price3=document.getElementById("priceM3");
+	var item4=document.getElementById("itemM4");
+	var price4=document.getElementById("priceM4");
+	item2.innerHTML=item3.innerHTML;
+	price2.innerHTML=price3.innerHTML;
+	item3.innerHTML=item4.innerHTML;
+	price3.innerHTML=price4.innerHTML;
+	item4.innerHTML="Item : Empty";
+	price4.innerHTML="0.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag--;
+}
+function reset3()
+{
+	var item3=document.getElementById("itemM3");
+	var price3=document.getElementById("priceM3");
+	price=price3.innerHTML;
+	tbill-=price;
+	var item4=document.getElementById("itemM4");
+	var price4=document.getElementById("priceM4");
+	item3.innerHTML=item4.innerHTML;
+	price3.innerHTML=price4.innerHTML;
+	item4.innerHTML="Item : Empty";
+	price4.innerHTML="0.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag--;
+}
+function reset4()
+{
+	var item4=document.getElementById("itemM4");
+	var price4=document.getElementById("priceM4");
+	price=price4.innerHTML;
+	tbill-=price;
+	item4.innerHTML="Item : Empty";
+	price4.innerHTML="0.00";
+	var access3=document.getElementById("totalbill");
+	access3.innerHTML = tbill+".00";
+	flag--;
+}
